86b05a006d9bef98b31e1aa6abd529a707300a38
Maggie Chen <maggieyudanchen@gmail.com>
2014-06-12 15:57:16 -0700

    Added login buttons

---
+++ src/login.php
@@ -0,0 +1,14 @@
+<p> This is the login page </p>
+
+<p> Please enter your username and password. </p>
+
+<p><form method = "POST" action = "checklogin.php">
+<p> Username <input type = "text" name = "username" size = 8> </p>
+<p> Password <input type = "password" name = "pw" size = 8> </p>
+<p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "submit" value = "Submit" name = "submit"></p>
+</form>
+</p>
+
+<?php
+
+?>
