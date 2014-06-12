70292c644d9d9018c0c2efc59909426c842ddb85
amelia-ylh <amelia_ylh@hotmail.com>
2014-06-12 16:44:30 -0700

    added a login_success page

---
+++ src/login_success.php
@@ -0,0 +1,15 @@
+
+// Check if session is not registered, redirect back to main page. 
+// Put this code in first line of web page. 
+<?php
+session_start();
+if(!session_is_registered(username)){
+header("location:santa-inc.php");
+}
+?>
+
+<html>
+<body>
+Login Successful
+</body>
+</html>
