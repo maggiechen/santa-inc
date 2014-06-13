508f295a6adc08cec6d19f617e808507c078fed4
Maggie Chen <maggieyudanchen@gmail.com>
2014-06-13 16:39:02 -0700

    Fixed most of checklogin.php...The if statements don't seem to be working

--- src/checklogin.php
+++ src/checklogin.php
@@ -1,6 +1,31 @@
 <?php
 // it is not necessary for you to use session to save variables, if you want to use 
 //session, you need to specify a save path
+
+//A note about OCI_Fetch_Array: After fetching the result, if you want to access an attribute, you have to use $result["ALLCAPS"] or it won't work
+
+function getUserAndPW($tab, $uname, $pw) {
+	global $A_name, $A_pwd;
+
+	$retval =	"select * 
+				from ".$tab."  where ".$uname." = '" .$A_name. "' and ".$pw." = 
+				'" .$A_pwd. "'";
+ 	//echo $retval."\r\n";
+	return $retval;
+}
+
+function printResult($result) { //prints results from a select statement
+	echo "<br>Got data from table<br>";
+	echo "<table>";
+	echo "<tr><th>Uname</th><th>Pw</th></tr>";
+
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+		echo "<tr><td>" . $row["IUNAME"] . "</td><td>" . $row["PW"] . "</td></tr>"; //or just use "echo $row[0]" 
+	}
+	echo "</table>";
+
+}
+
 ini_set('session.save_path','sessions');
 session_start(); 
 
@@ -12,88 +37,66 @@ $A_pwd=$_POST["pw"];    //receive password from previous form
 
 $oraconn = OCILogon("ora_f8l8", "a40626103", "ug");
 
+OCILogoff($db_conn);
+
+
 //Query the interns first
-function getUserAndPW($tab, $uname, $pw) {
-	global $A_name, $A_pwd;
-
-	return "select * 
-			from ".$tab."  where ".$uname." = '" .$A_name. "' and ".$pw." = 
-			'" .$A_pwd. "'";
-}
 
 
-
-/*
-$fulltimequery = "select * 
-			from FulltimeElf_mng_mon  where Funame = '" .$A_name. "' and pw = 
-			'" .$A_pwd. "'";
-$managerquery = "select * 
-			from ManagerElf  where Muname = '" .$A_name. "' and pw = 
-			'" .$A_pwd. "'";
-$unionquery = "select * 
-			from UnionWorker  where Uname = '" .$A_name. "' and pw = 
-			'" .$A_pwd. "'";
-
-$internquery = "select * 
-			from InternElf_train where Iuname = '" .$A_name. "' and pw = 
-			'" .$A_pwd. "'"; */
 $doquery = OCIParse($oraconn, getUserAndPW("InternElf_train", "Iuname", "pw")) or die("Couldn't parse statement.");
-OCIexecute($doquery) or die("Couldn't execute statement.");
+OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 $role = "intern";
 
 
-if (!OCIfetch($doquery)) {
+echo "after intern \r\n";
+printResult($doquery);
 
+
+if (!OCI_Fetch($doquery)) {
 	$doquery = OCIParse($oraconn, getUserAndPW("FulltimeElf_mng_mon", "Funame", "pw")) or die("Couldn't parse statement.");
-	OCIexecute($doquery) or die("Couldn't execute statement.");
+	OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 	$role = "fulltime";
+
+	echo "FULLTIME";
 } 
 
-if (!OCIfetch($doquery)){
+
+if (!OCI_Fetch($doquery)){
 	$doquery = OCIParse($oraconn, getUserAndPW("ManagerElf", "Muname", "pw")) or die("Couldn't parse statement.");
-	OCIexecute($doquery) or die("Couldn't execute statement.");
+	OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 	$role = "manager";
+	echo "MANAGER";
 }
 
-if (!OCIfetch($doquery)) {
+if (!OCI_Fetch($doquery)){
 	$doquery = OCIParse($oraconn, getUserAndPW("UnionWorker", "Uname", "pw")) or die("Couldn't parse statement.");
-	OCIexecute($doquery) or die("Couldn't execute statement.");
+	OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 	$role = "union";
-} else {
+	echo "UNIONWORKER";
+}
+
+if(!OCI_Fetch($doquery)){
    // the name and password are not in the table
-    echo "<script language='javascript'>alert('Your username and/or password are 
-wrong！Please enter again');history.back();</script>";
-            exit;
+  echo "Sorry, that's not a correct username/password combination.\r\n";
+  echo "You entered the username ".$A_name." and the password ".$A_pwd.".\r\n";
+   //header("location: login.php");
+
  }
 
-
- // the name and password are in the table
-switch (role) {
-	case "intern":  
-		$adminname = OCIresult($doquery, Iuname);
-		$password = OCIresult($doquery, pw);
-		break;
-	case "fulltime":
-		$adminname = OCIresult($doquery, Funame);
-		$password = OCIresult($doquery, pw);
-		break;
-	case "manager":
-		$adminname = OCIresult($doquery, Muname);
-		$password = OCIresult($doquery, pw);
-		break;
-	case "union":
-		$adminname = OCIresult($doquery, Uname);
-		$password = OCIresult($doquery, pw);
-		break;
+/* //Test query to see if parsing and executing an SQL statement works
+$tryquery = OCIParse($oraconn, "select * from InternElf_train");
+OCIexecute($tryquery, OCI_DEFAULT);
+if (OCIfetch($tryquery)) {
+	echo "Try worked";
+	printResult($tryquery);
 }
+*/
 
-
- 
  
    $_SESSION['admin_name']=$A_name;
-    $_SESSION['admin_pwd']=$A_pwd;
-  echo "<script language='javascript'>alert('Login 
-successfully!');window.location.href='homepage.php';</script>"; 
- }
+   $_SESSION['admin_pwd']=$A_pwd;
+  
+	//header("location: santa-inc.php");
+ 
  
 ?>
