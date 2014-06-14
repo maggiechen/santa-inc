32eca02e62ed195a0b09719605064006cded7051
amelia-ylh <amelia_ylh@hotmail.com>
2014-06-13 21:41:32 -0700

    making signup page pretty. Gunna work on login next mchen :P

--- src/signup.php
+++ src/signup.php
@@ -1,1 +1,25 @@
 <p> This is the signup page </p>
+
+<p id = "select-session-button">
+<form method="POST" action="checklogin.php">
+
+<p> Username <input type = "text" name = "username" size = 8> </p>
+<p> Password <input type = "password" name = "pw" size = 8> </p>
+
+<p> Type in your password again <input type = "password" name = "pw" size = 8> </p>
+
+<p>
+What kind of elf are you?
+<select name="item"> 
+<option>Manager Elf</option>
+<option>Intern Elf</option>
+<option>Fulltime Elf</option>
+<option>Union Worker</option>
+</p>
+
+
+<p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "submit" value = "Submit" name = 
+"submit"></p>
+
+</form>
+</p>
