e3df7be242c0b6110c092e0e6fd01ec0406b82c8
Maggie Chen <maggieyudanchen@gmail.com>
2014-06-13 18:36:43 -0700

    Still trying to debug ifs. Moved accidental logoff. Added table prints and echos

--- src/checklogin.php
+++ src/checklogin.php
@@ -10,7 +10,7 @@ function getUserAndPW($tab, $uname, $pw) {
 	$retval =	"select * 
 				from ".$tab."  where ".$uname." = '" .$A_name. "' and ".$pw." = 
 				'" .$A_pwd. "'";
- 	//echo $retval."\r\n";
+ 	echo "About to execute: ".$retval."\r\n";
 	return $retval;
 }
 
@@ -18,7 +18,7 @@ function printResult($result) { //prints results from a select statement
 	echo "<br>Got data from table<br>";
 	echo "<table>";
 	echo "<tr><th>Uname</th><th>Pw</th></tr>";
-
+//TODO: MUST CHANGE IUNAME IF YOU WANT TO SEE OTHER USERNAMES
 	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
 		echo "<tr><td>" . $row["IUNAME"] . "</td><td>" . $row["PW"] . "</td></tr>"; //or just use "echo $row[0]" 
 	}
@@ -37,7 +37,6 @@ $A_pwd=$_POST["pw"];    //receive password from previous form
 
 $oraconn = OCILogon("ora_f8l8", "a40626103", "ug");
 
-OCILogoff($db_conn);
 
 
 //Query the interns first
@@ -47,35 +46,37 @@ $doquery = OCIParse($oraconn, getUserAndPW("InternElf_train", "Iuname", "pw")) o
 OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 $role = "intern";
 
-
-echo "after intern \r\n";
+echo"<br>Intern table result:</br>";
 printResult($doquery);
 
-
-if (!OCI_Fetch($doquery)) {
+if (!OCIFetch($doquery)) {
 	$doquery = OCIParse($oraconn, getUserAndPW("FulltimeElf_mng_mon", "Funame", "pw")) or die("Couldn't parse statement.");
 	OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 	$role = "fulltime";
 
-	echo "FULLTIME";
+	echo"<br>Fulltime table result:</br>";
+	printResult($doquery);
+
 } 
 
 
-if (!OCI_Fetch($doquery)){
+if (!OCIFetch($doquery)){
 	$doquery = OCIParse($oraconn, getUserAndPW("ManagerElf", "Muname", "pw")) or die("Couldn't parse statement.");
 	OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 	$role = "manager";
-	echo "MANAGER";
+	echo"<br>Manager table result:</br>";
+	printResult($doquery);
 }
 
-if (!OCI_Fetch($doquery)){
+if (!OCIFetch($doquery)){
 	$doquery = OCIParse($oraconn, getUserAndPW("UnionWorker", "Uname", "pw")) or die("Couldn't parse statement.");
 	OCIexecute($doquery,OCI_DEFAULT) or die("Couldn't execute statement.");
 	$role = "union";
-	echo "UNIONWORKER";
+	echo"<br>Union table result:</br>";
+	printResult($doquery);
 }
 
-if(!OCI_Fetch($doquery)){
+if(!OCIFetch($doquery)){
    // the name and password are not in the table
   echo "Sorry, that's not a correct username/password combination.\r\n";
   echo "You entered the username ".$A_name." and the password ".$A_pwd.".\r\n";
@@ -91,6 +92,9 @@ if (OCIfetch($tryquery)) {
 	printResult($tryquery);
 }
 */
+echo "You are a ".$role;
+
+OCILogoff($db_conn);
 
  
    $_SESSION['admin_name']=$A_name;
