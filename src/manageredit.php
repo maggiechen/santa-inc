13f55c16b904f005048ac6611aedb8b0b2f47800
MrigankaArya <mrig.arya@gmail.com>
2014-06-18 18:52:50 -0700

    there seems to be a change but i swear theres none .-.

--- src/manageredit.php
+++ src/manageredit.php
@@ -144,6 +144,7 @@ if ($db_conn) {
 	if (array_key_exists('submitEUpdate', $_POST)) {
 		echo "update FulltimeElf_mng_mon set wages = '".$E_UWage."', insurance = '".$E_UIns."' where uname = '".$E_UPname."'";
 		executePlainSQL("update FulltimeElf_mng_mon set wages = '".$E_UWage."', insurance = '".$E_UIns."' where uname = '".$E_UName."'");
+>\
 		oci_commit($db_conn);
 	}
 
